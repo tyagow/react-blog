a40f6eca4558c36dc2f6424b319e9a92f3b872be
Tiago <tyagow@hotmail.com.br>
2018-03-30 20:40:31 -0300

    feat: Using BrowserRouter to Route urls with react-router-dom

--- blog/src/containers/App.js
+++ blog/src/containers/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from "react";
-import logo from "../logo.svg";
+import React from "react";
+import { Route } from "react-router-dom";
+
 import "./App.css";
-import { Provider } from "react-redux";
 
 import Posts from "../components/Posts";
 import PostForm from "../components/PostForm";
-import store from "../store";
-class App extends Component {
-  render() {
-    const categories = ["react", "js", "redux"];
-    return (
-      <Provider store={store}>
+
+const categories = ["react", "js", "redux"];
+
+const App = () => (
+  <div>
+    <Route
+      exact
+      path="/"
+      render={() => (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
           <PostForm categories={categories} />
           <hr />
           <Posts />
         </div>
-      </Provider>
-    );
-  }
-}
+      )}
+    />
+  </div>
+);
 
 export default App;
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,10 +1,21 @@
 import App from "./containers/App";
-import "bootstrap/dist/css/bootstrap.css";
-
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
