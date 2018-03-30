d38b0d0ea81353d8165c6959cf1e5f9f612f8404
Tiago Almeida <tyagow@hotmail.com.br>
2018-03-29 21:08:38 -0300

    feat: first commit, Component Posts created and fetching posts from api

---
+++ blog/src/components/Posts.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+
+class Posts extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
+    }
+  } 
+  componentDidMount() {
+    fetch('http://127.0.0.1:3001/posts',
+      { headers: { 'Authorization': 'ok' ,
+       dataType: 'json'
+    }})
+      .then(res => res.json())
+      .then(data => this.setState({
+        posts: data
+      }))
+  
+  }
+
+  render() {
+    const items = this.state.posts.map( post => (
+      <div key={post.id}>
+      <h2>{post.title}</h2>
+      </div>
+    ))
+    return (
+      <div>
+        <h1>Posts</h1>
+        {items}
+      </div>
+    )
+  }
+}
+
+export default Posts
