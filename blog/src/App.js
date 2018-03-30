587732e85e646ab50746905554d61230f06fb766
Tiago <tyagow@hotmail.com.br>
2018-03-30 12:24:32 -0300

    feat: PostForm component added to App.js

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
+import PostForm from "./components/PostsForm";
 
 class App extends Component {
   render() {
+    const categories = ["react", "js", "redux"];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <PostForm categories={categories} />
+        <hr />
         <Posts />
       </div>
     );
