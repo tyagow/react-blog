25bc82efa6319171dc979e7cbccfd659c3a958e1
Tiago <tyagow@hotmail.com.br>
2018-03-30 12:27:17 -0300

    refactor: Posts Component displaying more info from a post and eslint automatic file refactor

--- blog/src/components/Posts.js
+++ blog/src/components/Posts.js
@@ -1,38 +1,42 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
 
 class Posts extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       posts: []
-    }
-  } 
+    };
+  }
   componentDidMount() {
-    fetch('http://127.0.0.1:3001/posts',
-      { headers: { 'Authorization': 'ok' ,
-       dataType: 'json'
-    }})
+    fetch("http://127.0.0.1:3001/posts", {
+      headers: {
+        Authorization: "ok",
+        dataType: "json"
+      }
+    })
       .then(res => res.json())
-      .then(data => this.setState({
-        posts: data
-      }))
-  
+      .then(data =>
+        this.setState({
+          posts: data
+        })
+      );
   }
 
   render() {
-    const items = this.state.posts.map( post => (
+    const items = this.state.posts.map(post => (
       <div key={post.id}>
-      <h2>{post.title}</h2>
+        <h2>{post.title}</h2>
+        <div>Author:{post.author}</div>
+        <p>{post.body}</p>
       </div>
-    ))
+    ));
     return (
       <div>
         <h1>Posts</h1>
         {items}
       </div>
-    )
+    );
   }
 }
 
-export default Posts
+export default Posts;
