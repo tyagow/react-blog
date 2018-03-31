5d593556df36614fc062a13a7f8278448f42e160
Tiago <tyagow@hotmail.com.br>
2018-03-31 13:29:46 -0300

    refactor: styles now are in a separeted file

---
+++ blog/src/styles.js
@@ -0,0 +1,12 @@
+const styleMap = {
+  categoryLabel: {
+    style: {
+      border: "0",
+      color: "white",
+      padding: "-10px",
+      backgroundColor: "red"
+    }
+  }
+};
+
+export default styleMap;
