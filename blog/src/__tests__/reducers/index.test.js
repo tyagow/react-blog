a53e528de6c3b0da94505b15f4bf1746b74c449a
Tiago Almeida <tyagow@hotmail.com.br>
2018-04-09 04:54:10 -0300

    test: refactor folder of tests to __tests__

--- blog/src/__tests__/reducers/index.test.js
+++ blog/src/__tests__/reducers/index.test.js
@@ -1,9 +1,13 @@
 import rootReducer from "../../reducers/index";
 import PostReducer from "../../reducers/PostReducer";
+import CommentReducer from "../../reducers/CommentReducer";
 
 describe("Testing Combined Reducers", () => {
   it("index.js must export posts and comments as default combinedReducers", () => {
-    const expectedReducers = { posts: PostReducer(undefined, {}) };
+    const expectedReducers = {
+      posts: PostReducer(undefined, {}),
+      comments: CommentReducer(undefined, {})
+    };
     expect(rootReducer(undefined, {})).toEqual(expectedReducers);
   });
 });
