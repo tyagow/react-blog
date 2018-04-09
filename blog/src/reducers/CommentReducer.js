a17b0fa072d7b502b103e6c51829a086a4cd3098
Tiago <tyagow@hotmail.com.br>
2018-04-09 17:50:59 -0300

    tests: refactor tests folder ... strage things when using __tests__ folder ...

---
+++ blog/src/reducers/CommentReducer.js
@@ -0,0 +1,64 @@
+import {
+  FETCH_POSTS_SUCCESS,
+  NEW_POST,
+  GET_POST_BY_ID,
+  FETCH_CATEGORIES,
+  FETCH_COMMENTS,
+  FETCHING_POSTS,
+  FETCH_POSTS_FAILURE
+} from "../actions/ActionTypes";
+
+const initialState = {
+  items: [],
+  item: {},
+  postDetail: {},
+  categories: [],
+  fetching: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCHING_POSTS:
+      return {
+        ...state,
+        fetching: true
+      };
+    case FETCH_POSTS_SUCCESS:
+      return {
+        ...state,
+        items: action.payload,
+        fetching: false
+      };
+    case FETCH_POSTS_FAILURE:
+      return {
+        ...state,
+        fetching: false
+      };
+    case NEW_POST:
+      return {
+        ...state,
+        item: action.payload
+      };
+
+    case GET_POST_BY_ID:
+      return {
+        ...state,
+        postDetail: action.payload
+      };
+
+    case FETCH_CATEGORIES:
+      return {
+        ...state,
+        categories: action.payload
+      };
+
+    case FETCH_COMMENTS:
+      return {
+        ...state,
+        comments: action.payload
+      };
+
+    default:
+      return state;
+  }
+}
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import PostReducer from "./PostReducer";
+import CommentReducer from "./CommentReducer";
 
 export default combineReducers({
-  posts: PostReducer
+  posts: PostReducer,
+  comments: CommentReducer
 });
