dd1a747d1c153f904645a33c69da50678e227658
Tiago Almeida <tyagow@hotmail.com.br>
2018-04-09 21:06:34 -0300

    refactor: using ES6 pattern of a arrow function with just return

--- blog/src/containers/HomePage.js
+++ blog/src/containers/HomePage.js
@@ -2,14 +2,12 @@ import React from "react";
 import PostForm from "../components/posts/PostForm";
 import Posts from "../components/posts/Posts";
 
-export const HomePage = () => {
-  return (
-    <div className="container">
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
-  );
-};
+export const HomePage = () => (
+  <div className="container">
+    <PostForm />
+    <hr />
+    <Posts />
+  </div>
+);
 
 export default HomePage;
--- blog/src/containers/PostDetailPage.js
+++ blog/src/containers/PostDetailPage.js
@@ -2,13 +2,11 @@ import React from "react";
 import PostDetail from "../components/posts/PostDetail";
 import Comments from "../components/comments/Comments";
 
-const PostDetailPage = props => {
-  return (
-    <div className="">
-      <PostDetail postId={props.match.params.postId} />
-      <Comments postId={props.match.params.postId} />
-    </div>
-  );
-};
+const PostDetailPage = props => (
+  <div className="post-detail-page-container">
+    <PostDetail postId={props.match.params.postId} />
+    <Comments postId={props.match.params.postId} />
+  </div>
+);
 
 export default PostDetailPage;
