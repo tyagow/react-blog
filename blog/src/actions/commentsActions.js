d5aa6a4004dd561e74c7283ddfed206dec05e4c0
Tiago <tyagow@hotmail.com.br>
2018-04-13 12:00:30 -0300

    feat: creating setComments function to dispatch action after fetch posts

--- blog/src/actions/commentsActions.js
+++ blog/src/actions/commentsActions.js
@@ -1,16 +1,17 @@
-import { FETCH_COMMENTS, API } from "./actionTypes";
+import { FETCH_COMMENTS_SUCCESS, API } from "./actionTypes";
 
 export const fetchCommentsByPostId = postId => dispatch => {
   dispatch({
     type: API,
     payload: {
       url: "http://127.0.0.1:3001/posts/" + postId + "/comments",
-      success: comments =>
-        dispatch({
-          type: FETCH_COMMENTS,
-          payload: comments
-        }),
+      success: setComments,
       label: "comments_list"
     }
   });
 };
+
+export const setComments = comments => ({
+  type: FETCH_COMMENTS_SUCCESS,
+  payload: comments
+});
