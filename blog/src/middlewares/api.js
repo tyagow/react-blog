aa5b6f94c34849af5c45fcaf80d4a623694c129a
Tiago Almeida <tyagow@hotmail.com.br>
2018-04-12 21:10:25 -0300

    feat: new middleware api responsible for handling async api requests

---
+++ blog/src/middlewares/api.js
@@ -0,0 +1,42 @@
+import * as types from "../actions/actionTypes";
+
+import { startNetwork, endNetwork } from "../actions/ui";
+
+const api = ({ dispatch, getState }) => next => action => {
+  if (action.type !== types.API) {
+    return next(action);
+  }
+
+  const {
+    url,
+    success,
+    label,
+    options,
+    method = "get",
+    body = undefined
+  } = action.payload;
+
+  dispatch(startNetwork(label));
+  let options_header = {
+    ...options,
+    headers: {
+      Authorization: "ok",
+      dataType: "json",
+      "content-type": "application/json"
+    },
+    method: method,
+    body: body
+  };
+  fetch(url, options_header)
+    .then(response => response.json())
+    .then(data => {
+      dispatch(success(data));
+      dispatch(endNetwork(label));
+    })
+    .catch(error => {
+      console.error(error);
+      dispatch(endNetwork(label));
+    });
+};
+
+export default api;
