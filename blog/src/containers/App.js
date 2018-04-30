2b5a06dca9bf48dee2f18afc84ec5740aec17bbf
Tiago Almeida <tyagow@hotmail.com.br>
2018-04-30 10:34:00 -0300

    feat: new route to create new post

--- blog/src/containers/App.js
+++ blog/src/containers/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 import HomePage from "./HomePage";
 import PostDetailPage from "./PostDetailPage";
+import { PostForm } from "../components/posts/PostForm";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" render={() => <HomePage />} />
+          <Route exact path="/posts/create" render={() => <PostForm />} />
           <Route
             exact
             path="/:category/:postId"
