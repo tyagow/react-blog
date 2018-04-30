c91f40da9127239a0bff4083a2635af8a21c28ad
Tiago Almeida <tyagow@hotmail.com.br>
2018-04-30 10:32:05 -0300

    refactor: creating button to create a post

--- blog/src/components/posts/Posts.js
+++ blog/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import PostLabel from "./PostLabel";
 import { getPosts, getCategories } from "../../actions/postActions";
+import Link from "react-router-dom/Link";
 
 export class Posts extends Component {
   componentDidMount = () => {
@@ -20,6 +21,12 @@ export class Posts extends Component {
     return (
       <div>
         <h1>Posts</h1>
+        <hr />
+
+        <Link to="posts/create">
+          <button className="btn btn-info posts-create">Create Post</button>
+        </Link>
+        <br />
         {items}
       </div>
     );
