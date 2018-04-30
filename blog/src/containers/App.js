532e4b940a3be081cd3c40cf94f9a35344b19e98
Tiago <tyagow@hotmail.com.br>
2018-04-30 17:21:16 -0300

    refactor: removing redirect middleware

--- blog/src/containers/App.js
+++ blog/src/containers/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import NotFoundPage from "./NotFoundPage";
 import HomePage from "./HomePage";
 import PostDetailPage from "./PostDetailPage";
-import { PostForm } from "../components/posts/PostForm";
+import PostForm from "../components/posts/PostForm";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Switch>
-          <Route exact path="/" render={() => <HomePage />} />
-          <Route exact path="/posts/create" render={() => <PostForm />} />
-          <Route
-            exact
-            path="/:category/:postId"
-            render={({ match }) => <PostDetailPage match={match} />}
-          />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" render={() => <HomePage />} />
+            <Route exact path="/posts/create" render={() => <PostForm />} />
+            <Route
+              exact
+              path="/:category/:postId"
+              render={({ match }) => <PostDetailPage match={match} />}
+            />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- blog/src/middlewares/redirect.js
+++ blog/src/middlewares/redirect.js
@@ -1,10 +1,10 @@
-import { browserHistory } from "react-router";
-
 const redirect = ({ dispatch, getState }) => next => action => {
   if (action.redirect) {
-    browserHistory.push(action.redirect);
+    // browserHistory.push(action.redirect);
+    console.log(action.redirect);
     return next(action);
   }
+  return next(action);
 };
 
 export default redirect;
