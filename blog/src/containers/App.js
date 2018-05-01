bf3902dd4136b1dc07af27664a966abcd0493069
Tiago <tyagow@hotmail.com.br>
2018-05-01 10:23:26 -0300

    refactor: removinf redundant BrowserHistory

--- blog/src/containers/App.js
+++ blog/src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import NotFoundPage from "./NotFoundPage";
 import HomePage from "./HomePage";
@@ -9,20 +9,18 @@ import PostForm from "../components/posts/PostForm";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route exact path="/" render={() => <HomePage />} />
-            <Route exact path="/posts/create" render={() => <PostForm />} />
-            <Route
-              exact
-              path="/:category/:postId"
-              render={({ match }) => <PostDetailPage match={match} />}
-            />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => <HomePage />} />
+          <Route exact path="/posts/create" render={() => <PostForm />} />
+          <Route
+            exact
+            path="/:category/:postId"
+            render={({ match }) => <PostDetailPage match={match} />}
+          />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
     );
   }
 }
