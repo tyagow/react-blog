67525a7d656e89872d7a22135145332e123d9f48
Tiago Almeida <tyagow@hotmail.com.br>
2018-05-18 20:31:30 -0300

    test: creating test for Header if call getCategories action on mount

--- blog/src/tests/components/Header.test.js
+++ blog/src/tests/components/Header.test.js
@@ -2,14 +2,20 @@ import React from "react";
 import { shallow } from "enzyme";
 import { Header } from "../../components/ui/Header";
 function setup() {
-  const enzymeWrapper = shallow(<Header />);
+  const getCategories = jest.fn();
+  const enzymeWrapper = shallow(<Header getCategories={getCategories} />);
   return {
-    enzymeWrapper
+    enzymeWrapper,
+    getCategories
   };
 }
 describe("Header", () => {
   it("renders Header without crashing", () => {
     const { enzymeWrapper } = setup();
     expect(enzymeWrapper).toMatchSnapshot();
+  });
+  it("should call getCategories when componentDidMount lifecycle calls", () => {
+    const { getCategories } = setup();
+    expect(getCategories).toBeCalled();
   });
 });
