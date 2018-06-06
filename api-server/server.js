be4230025d42edf1369065536b5f62b6a79d4dd9
Tiago Almeida <tyagow@hotmail.com.br>
2018-06-06 20:36:49 -0300

    refactor: changing path to serve static files

--- api-server/server.js
+++ api-server/server.js
@@ -1,21 +1,20 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const cors = require('cors')
-const config = require('./config')
-const categories = require('./categories')
-const posts = require('./posts')
-const comments = require('./comments')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const config = require("./config");
+const categories = require("./categories");
+const posts = require("./posts");
+const comments = require("./comments");
 
-const app = express()
+const app = express();
 
-app.use(express.static('../blog/build'))
-app.use(cors())
+app.use(express.static(path.resolve(__dirname, "../blog/build")));
+app.use(cors());
 
-
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   const help = `
   <pre>
     Welcome to the Udacity Readable API!
@@ -110,216 +109,204 @@ app.get('/api', (req, res) => {
       USAGE:
         Sets a comment's deleted flag to 'true'
  </pre>
-  `
+  `;
 
-  res.send(help)
-})
+  res.send(help);
+});
 
-app.use('/api/*', (req, res, next) => {
-  const token = req.get('Authorization')
+app.use("/api/*", (req, res, next) => {
+  const token = req.get("Authorization");
 
   if (token) {
-    req.token = token
-    next()
+    req.token = token;
+    next();
   } else {
     res.status(403).send({
-      error: 'Please provide an Authorization header to identify yourself (can be whatever you want)'
-    })
+      error:
+        "Please provide an Authorization header to identify yourself (can be whatever you want)"
+    });
   }
-})
+});
 
+app.get("/api/categories", (req, res) => {
+  categories.getAll(req.token).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.get('/api/categories', (req, res) => {
-    categories.getAll(req.token)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.get("/api/:category/posts", (req, res) => {
+  posts.getByCategory(req.token, req.params.category).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.get('/api/:category/posts', (req, res) => {
-    posts.getByCategory(req.token, req.params.category)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.get("/api/posts", (req, res) => {
+  posts.getAll(req.token).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.get('/api/posts', (req, res) => {
-    posts.getAll(req.token)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                 error: 'There was an error.'
-          })
-        }
-      )
-})
+app.post("/api/posts", bodyParser.json(), (req, res) => {
+  posts.add(req.token, req.body).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.post('/api/posts', bodyParser.json(), (req, res) => {
-    posts.add(req.token, req.body)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                 error: 'There was an error.'
-          })
-        }
-      )
-})
+app.get("/api/posts/:id", (req, res) => {
+  posts.get(req.token, req.params.id).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.get('/api/posts/:id', (req, res) => {
-    posts.get(req.token, req.params.id)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.delete("/api/posts/:id", (req, res) => {
+  posts
+    .disable(req.token, req.params.id)
+    .then(post => comments.disableByParent(req.token, post))
+    .then(
+      data => res.send(data),
+      error => {
+        console.error(error);
+        res.status(500).send({
+          error: "There was an error."
+        });
+      }
+    );
+});
 
-app.delete('/api/posts/:id', (req, res) => {
-    posts.disable(req.token, req.params.id)
-      .then(post => comments.disableByParent(req.token, post))
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.post("/api/posts/:id", bodyParser.json(), (req, res) => {
+  const { option } = req.body;
+  const id = req.params.id;
+  posts.vote(req.token, id, option).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.post('/api/posts/:id', bodyParser.json(), (req, res) => {
-    const { option } = req.body
-    const id = req.params.id
-    posts.vote(req.token, id, option)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.put("/api/posts/:id", bodyParser.json(), (req, res) => {
+  posts.edit(req.token, req.params.id, req.body).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.put('/api/posts/:id', bodyParser.json(), (req, res) => {
-    posts.edit(req.token, req.params.id, req.body)
-      .then(
-        (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.get("/api/posts/:id/comments", (req, res) => {
+  comments.getByParent(req.token, req.params.id).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.get('/api/posts/:id/comments', (req, res) => {
-    comments.getByParent(req.token, req.params.id)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.get("/api/comments/:id", (req, res) => {
+  comments.get(req.token, req.params.id).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.get('/api/comments/:id', (req, res) => {
-    comments.get(req.token, req.params.id)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.put("/api/comments/:id", bodyParser.json(), (req, res) => {
+  comments.edit(req.token, req.params.id, req.body).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.put('/api/comments/:id', bodyParser.json(), (req, res) => {
-    comments.edit(req.token, req.params.id, req.body)
-      .then(
-        (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.post("/api/comments", bodyParser.json(), (req, res) => {
+  comments.add(req.token, req.body).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.post('/api/comments', bodyParser.json(), (req, res) => {
-    comments.add(req.token, req.body)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
+app.post("/api/comments/:id", bodyParser.json(), (req, res) => {
+  const { option } = req.body;
+  comments.vote(req.token, req.params.id, option).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
 
-app.post('/api/comments/:id', bodyParser.json(), (req, res) => {
-    const { option } = req.body
-    comments.vote(req.token, req.params.id, option)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
-
-app.delete('/api/comments/:id', (req, res) => {
-    comments.disable(req.token, req.params.id)
-      .then(
-          (data) => res.send(data),
-          (error) => {
-              console.error(error)
-              res.status(500).send({
-                  error: 'There was an error.'
-              })
-          }
-      )
-})
-if (process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../blog', 'build', 'index.html'))
-    })
+app.delete("/api/comments/:id", (req, res) => {
+  comments.disable(req.token, req.params.id).then(
+    data => res.send(data),
+    error => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error."
+      });
+    }
+  );
+});
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../blog", "build", "index.html"));
+  });
 }
 
 app.listen(config.port, () => {
-  console.log('Server listening on port %s, Ctrl+C to stop', config.port)
-})
+  console.log("Server listening on port %s, Ctrl+C to stop", config.port);
+});
